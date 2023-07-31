dfb7072d5eb8a057c07a95cc9b1b9374e1f619ea
Alfredo Braga <alfredorodruguesbraga@gmail.com>
2023-07-31 17:00:09 -0300

    melhorando a view

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/users', status_code=200, response_model=UserList)
+def all_users():
+    return {'users': database}
+
+
+@app.post('/users', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return UserPublic(**user_with_id.model_dump())
+
+
+@app.put('/users/{id}', status_code=201, response_model=UserPublic)
+def update_user(id: int, user: UserSchema):
+    if id > len(database) or id < 1:
+        raise HTTPException(status_code=404, detail='Not Found')
+
+    user_with_id = UserDB(**user.model_dump(), id=id)
+    database[id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete('/users/{id}', status_code=200, response_model=Message)
+def delete_user(id: int):
+
+    if id > len(database) or id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    del database[id - 1]
+
+    return {'detail': 'Usuário removido com sucesso'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+
 from fast_zero.app import app
 
 client = TestClient(app)
@@ -8,3 +9,76 @@ def test_root_deve_retornar_200_e_ola_mundo():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_one_plus_one():
+    assert 1 + 1 == 2
+
+
+def test_read_users():
+    response = client.get('/users')
+
+    assert response.status_code == 200
+
+    assert response.json()
+
+
+def test_create_user():
+    response = client.post(
+        '/users',
+        json={
+            'email': 'alphabraga@hotmail.com',
+            'username': 'alphabraga',
+            'password': '123456',
+        },
+    )
+
+    assert response.status_code == 201
+
+
+def test_update_user_must_return_404():
+    response = client.put(
+        '/usuers/0',
+        json={
+            'email': 'alphabraga@hotmail.com',
+            'username': 'alphabraga',
+            'password': '123456',
+        },
+    )
+
+    assert response.status_code == 404
+
+
+def test_update_user():
+
+    response = client.post(
+        '/users',
+        json={
+            'email': 'alphabraga@hotmail.com',
+            'username': 'alphabraga',
+            'password': '123456',
+        },
+    )
+
+    assert response.status_code == 201
+    user = response.json()
+    new_id = user.get('id')
+    assert new_id == 2
+
+    response = client.put(
+        f'/users/{new_id}',
+        json={
+            'email': 'alphabragaUPDATED@hotmail.com',
+            'username': 'alphabragaUPDATED',
+            'password': '123456',
+        },
+    )
+
+    assert response.status_code == 201
+    assert response.json().get('username') == 'alphabragaUPDATED'
+
+
+def test_remove_user():
+
+    response = client.delete('/users/1')
+    assert response.status_code == 200
